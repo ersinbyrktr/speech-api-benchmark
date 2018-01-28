21004b5a3d718a979b87ea8eb443fee026a39344
Josquin Cornec <josquin@victoria.frmug.org>
2018-01-28 15:24:51 +0100

    Create a new class which compute the distance between 2 string and specifies insertion/deletion/substitution

---
+++ src/main/java/utility/ApproximateStringMatching.java
@@ -0,0 +1,110 @@
+package utility;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author josquin
+ */
+public class ApproximateStringMatching {
+
+    //Initialize Map
+    private static Map<String, Integer> answer = new HashMap<>();
+    //Create a tab which store the weight
+    private static int weight[][];
+
+    public static void main(String[] args){
+        String reference = "hello aze";
+        String output = "hellos baze";
+        computeDistance(reference, output);
+    }
+
+    public static Map<String, Integer> computeDistance(String reference, String output){
+
+
+        answer.put("Insertion", 0);
+        answer.put("Deletion", 0);
+        answer.put("Substitution", 0);
+
+
+        //Initialization
+        weight = new int[reference.length()+1][output.length()+1];
+        for(int i=0; i<weight.length; weight[i][0]=i++);
+        for(int i=0; i<weight[0].length; weight[0][i]=i++);
+
+        //Compute the weight
+        for(int i=1; i<weight.length; i++) {
+            for (int j = 1; j < weight[0].length; j++) {
+                int cost = reference.charAt(i-1) == output.charAt(j-1) ? 0 : 1;
+                weight[i][j] = Math.min(weight[i - 1][j - 1] + cost, Math.min(weight[i - 1][j]+1, weight[i][j - 1]+1));
+            }
+        }
+        answer.put("Total", weight[weight.length-1][weight[0].length-1]);
+
+        //Travel the matrix recursively to see if it is an insertion, deletion or substitution
+        backwardWalking(weight.length-1, weight[0].length-1);
+
+        printWeight();
+        printResult(answer);
+
+        return answer;
+    }
+
+    private static void backwardWalking(int i, int j){
+        if(i==0 && j==0) return;
+        int diagonal;
+        int horizontal;
+        int vertical;
+
+        try {
+            diagonal = weight[i - 1][j - 1];
+        }catch (ArrayIndexOutOfBoundsException e){
+            diagonal = weight[weight.length-1][weight[0].length-1];
+        }
+        try{
+            horizontal = weight[i][j - 1];
+        }catch (ArrayIndexOutOfBoundsException e){
+            horizontal = weight[weight.length-1][weight[0].length-1];
+        }
+        try{
+            vertical = weight[i - 1][1];
+        }catch (ArrayIndexOutOfBoundsException e){
+            vertical = weight[weight.length-1][weight[0].length-1];
+        }
+
+        int min = Math.min(diagonal, Math.min(horizontal, vertical));
+
+        //Diagonal
+        if(min==diagonal){
+            if(min!=weight[i][j]) answer.put("Substitution", answer.get("Substitution")+1);
+            j--;
+            i--;
+        }
+        //Horizontal
+        else if(min==horizontal){
+            answer.put("Insertion", answer.get("Insertion")+1);
+            j--;
+        }
+        //Vertical
+        else if(min==vertical){
+            answer.put("Deletion", answer.get("Deletion")+1);
+            i--;
+        }
+        backwardWalking(i, j);
+    }
+
+    private static void printWeight(){
+        for(int i=0; i<weight.length; i++) {
+            for (int j = 0; j < weight[0].length; j++) {
+                System.out.print(weight[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printResult(Map<String, Integer> map){
+        for (String s: map.keySet()) {
+            System.out.println(s+" : "+map.get(s));
+        }
+    }
+}
